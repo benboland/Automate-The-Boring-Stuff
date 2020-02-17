4889e25648c617ee5af15a9f1eadcaf8c6359e8d
Benjamin Boland <benjamin@bo.land>
2020-02-16 21:13:21 -0800

    3

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -10,14 +10,31 @@ LOGO_FILENAME = 'catlogo.png'
 logoIm = Image.open(LOGO_FILENAME)
 logoWidth, logoHeight = logoIm.size
 
-# TODO: Loop over all files in the working directory
+os.makedirs('withLogo', exist_ok=True)
+# loop over all files in the working directory
+for filename in os.listdir('.'):
+    if not (filename.endswith('.png') or filename.endswith('.jpg')) or filename == LOGO_FILENAME:
+        continue        # skip non-image files and the logo file itself
+    im = Image.open(filename)
+    width, height = im.size
 
-# TODO: Check if image needs to be resized
+# check if image needs to be resized
+if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+    # calculate  the new width and height to resize to
+    if width > height:
+        height = int((SQUARE_FIT_SIZE / width) * height)
+        width = SQUARE_FIT_SIZE
+    else:
+        width = int((SQUARE_FIT_SIZE / height) * width)
+        height = SQUARE_FIT_SIZE
 
-# TODO: Calculate the new width and height to resize to
+# resize the image
+print('Resizing %s...' % (filename))
+im = im.resize((width, height))
 
-# TODO: Resize the image
+# add the logo
+print('Adding logo to %s...' % (filename))
+im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
 
-# TODO: Add the logo
-
-# TODO: Save changed
+# save changed
+im.save(os.path.join('withLogo', filename))
